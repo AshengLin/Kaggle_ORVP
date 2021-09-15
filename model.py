1cdb91ebae26a9124b7b3e40368729efc750bbaa
Derek <a0988238060@gmail.com>
2021-09-15 23:16:23 +0800

    tuning 0915

--- model.py
+++ model.py
@@ -16,12 +16,14 @@ test_x, test_y = data_x[400000:], data_y[400000:]
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(11,)),
   tf.keras.layers.Dense(10, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(7, activation='relu'),
+  # tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(5, activation='tanh'),
+  tf.keras.layers.Dense(3, activation='tanh'),
   tf.keras.layers.Dense(1, activation='tanh')
 ])
 
-adam = keras.optimizers.Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+adam = keras.optimizers.Adam(lr=0.0000001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 model.compile(optimizer=adam,
               loss='mean_squared_error')
 
@@ -29,4 +31,5 @@ model.fit(train_x, train_y, epochs=100)
 model.evaluate(test_x, test_y)
 
 pred = model.predict(test_x)
+print(pred)
 
